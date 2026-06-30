8b5443ada5acbf845557d06bf9a456ff13b4ccba
Magmowy <luki.drapek@o2.pl>
2026-06-30 11:07:03 +0200

    Main setup. Initializing game, and choosing difficulty and length

--- Program.cs
+++ Program.cs
@@ -11,6 +11,30 @@ You should record previous games in a List and there should be an option in the 
 
 You don't need to record results on a database. Once the program is closed the results will be deleted.
 
+Try to implement levels of difficulty.
+
+Add a timer to track how long the user takes to finish the game.
+
 
 */
 
+static void MathGame(string welcomeMessage="--Math game--", int difficulty = 0, int gameLength = 5){
+    Console.WriteLine(welcomeMessage);
+    Console.WriteLine("Difficulty: " + difficulty);
+    Console.WriteLine("Game length: " + gameLength);
+}
+string message = "--Math game--";
+Console.WriteLine("Difficulty: 0-5");
+int difficulty = int.Parse(Console.ReadLine());
+if (difficulty > 5){
+    difficulty = 5;
+}
+if (difficulty < 0){
+    difficulty = 0;
+}
+Console.WriteLine("Game length: 1 minimum:");
+int gameLength = int.Parse(Console.ReadLine());
+if (gameLength < 1){
+    gameLength = 1;
+}
+MathGame(message, difficulty, gameLength);
